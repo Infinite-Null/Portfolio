dab1947af63cfe16d9a22f543f8eb77b83092754
Infinite-Null <ankit.kum.sha9933@gmail.com>
2024-06-29 23:09:22 +0530

    Added Projects

--- src/Components/Work/EachWorkCard.jsx
+++ src/Components/Work/EachWorkCard.jsx
@@ -22,21 +22,12 @@ function EachWorkCard({name, description, skills, github, live, image, index}) {
                 }
             }}
             className="flex bg-white rounded-lg mb-5hover:shadow-xl p-6">
-            <div className="rotate-180 p-2 [writing-mode:_vertical-lr]">
-                <time
-                    dateTime="2022-10-10"
-                    className="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900"
-                >
-                    <span>2022</span>
-                    <span className="w-px flex-1 bg-gray-900/10"></span>
-                    <span>Oct 10</span>
-                </time>
-            </div>
+
 
             <div className="hidden sm:block sm:basis-56">
                 <img
                     alt=""
-                    src="https://private-user-images.githubusercontent.com/97950192/329802952-8da0020e-45ef-46f4-9724-8e38ceef9447.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTk1MTUyODIsIm5iZiI6MTcxOTUxNDk4MiwicGF0aCI6Ii85Nzk1MDE5Mi8zMjk4MDI5NTItOGRhMDAyMGUtNDVlZi00NmY0LTk3MjQtOGUzOGNlZWY5NDQ3LmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA2MjclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNjI3VDE5MDMwMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTdjM2ZlMGFlMGQ5NmQ2NDY2ZDY0OTczMjI2ZWI1YTEyYjk3ZjIxYjdmZTMwM2E4OWE1ZjM1MGMyYmUwOWYyMjYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.tCPEvAn0vVXtJFPjZW_DpSmFCtgOQmh5eVSXeEgRYfU"
+                    src={image}
                     className="aspect-square h-full w-full object-cover rounded-lg"
                 />
             </div>
@@ -65,6 +56,7 @@ function EachWorkCard({name, description, skills, github, live, image, index}) {
                 <div
                     className="sm:flex sm:items-end sm:justify-start pl-8 gap-2">
                     <a
+                        target={"_blank"}
                         onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}
                         href={github}
                         className="flex items-center justify-center bg-[#3e3e3e] border-[#3e3e3e] rounded-full border-2 px-5 py-3 text-center text-xs font-bold uppercase transition hover:bg-black text-white duration-500 gap-2"
@@ -73,6 +65,7 @@ function EachWorkCard({name, description, skills, github, live, image, index}) {
                     </a>
                     <div className="w-px h-1 bg-gray-900/10 sm:hidden"></div>
                     <a
+                        target={"_blank"}
                         onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}
                         href={live}
                         className="flex items-center justify-center bg-[#3e3e3e] border-[#3e3e3e] rounded-full border-2 px-5 py-3 text-center text-xs font-bold uppercase transition hover:bg-black text-white duration-500 gap-2"
--- src/Screens/Work.jsx
+++ src/Screens/Work.jsx
@@ -4,53 +4,74 @@ import {useContext} from "react";
 import {useState} from "react";
 import {Social} from "../Utilities/Links.js";
 import {motion} from "framer-motion";
-
+import ecommerce from "../assets/Projects/ecommerce.png"
+import melody from "../assets/Projects/melody.png"
+import nekoflix from "../assets/Projects/Nekoflix.png"
+import fungrow from "../assets/Projects/fungrow.png"
+import o2 from "../assets/Projects/O2Ai.png"
+import outofkind from "../assets/Projects/Outofkind.png"
 function Work() {
     const {mouseEnter, mouseLeave} = useContext(Context)
     const [Active, setActive] = useState(1)
-    // TODO: Add Real projects
     const WorkData = [
         {
             name:"Melody: Music Player",
-            description:"Your Ultimate Ad-Free Music Companion! Developed with React Native and harnessing the power of JioSavan's Unofficial API, Melody offers you a seamless music streaming experience at your fingertips.",
-            skills:["React", "Tailwind CSS", "React Icons"],
-            github:Social.Github,
-            live:Social.Github
+            description:"Introducing Melody: Your Ultimate Ad-Free Music Companion! Developed with React Native and harnessing the power of JioSavan's Unofficial API, Melody offers you a seamless music streaming experience at your fingertips. Dive into a world of limitless tunes, curated playlists, and personalized recommendations—all without interruptions",
+            skills:[
+                "application",
+                "react-native",
+                "online-music-player",
+                "music-app",
+                "react-native-reanimated",
+            ],
+            github:"https://github.com/Infinite-Null/Melody",
+            live:"https://melody-landing-page-one.vercel.app/",
+            image:melody
         },
         {
-            name:"Melody: Music Player",
-            description:"Your Ultimate Ad-Free Music Companion! Developed with React Native and harnessing the power of JioSavan's Unofficial API, Melody offers you a seamless music streaming experience at your fingertips.",
-            skills:["React", "Tailwind CSS", "React Icons"],
-            github:Social.Github,
-            live:Social.Github
+            name:"Nekoflix: Anime and Manga Haven!",
+            description:"Introducing Nekoflix, your all-in-one destination for anime and manga enthusiasts! With Nekoflix, users can seamlessly stream their favorite anime and dive into captivating manga series, all without interruptions and completely free of charge. Our sleek and user-friendly interface ensures a delightful experience ",
+            skills:[
+                "application",
+                "react-native",
+                "react-native-reanimatedv2",
+                "Rest-API",
+            ],
+            github:"https://github.com/Infinite-Null/NekoFlix",
+            live:"https://nekoflix-landing-page.vercel.app/",
+            image:nekoflix
         },
         {
-            name:"Melody: Music Player",
-            description:"Your Ultimate Ad-Free Music Companion! Developed with React Native and harnessing the power of JioSavan's Unofficial API, Melody offers you a seamless music streaming experience at your fingertips.",
-            skills:["React", "Tailwind CSS", "React Icons"],
-            github:Social.Github,
-            live:Social.Github
+            name:"Ecommerce: Full-Stack Ecommerce Website!",
+            description:"A responsive e-commerce website built with the MERN stack and JWT authentication. It includes secure user login, product management, shopping cart, and order management, using technologies like React, Node.js, Express, MongoDB, and Tailwind CSS.",
+            skills:["Full Stack","React", "Tailwind CSS", "MongoDB", "Express", "Node.js", "JWT Authentication"],
+            github:"https://github.com/Infinite-Null/E-Commerce/",
+            live:"https://ecommerce-frontend-phi-three.vercel.app/",
+            image:ecommerce
         },
         {
-            name:"Melody: Music Player",
-            description:"Your Ultimate Ad-Free Music Companion! Developed with React Native and harnessing the power of JioSavan's Unofficial API, Melody offers you a seamless music streaming experience at your fingertips.",
-            skills:["React", "Tailwind CSS", "React Icons"],
-            github:Social.Github,
-            live:Social.Github
+            name:"Out of Kind: Full-Stack Donation Website!",
+            description:"In this platform user can donate by listing it in the platform and the person who needs it will collect it from him, Even user can get the information for NOG's near them by heading to search section. Finally we have a community section where user can share any kind story to motivate others. ",
+            skills:["Next.js", "Tailwind CSS", "MongoDB", "Express", "Node.js", "JWT Authentication"],
+            github:"https://github.com/Infinite-Null/OutOfKind",
+            live:"https://out-of-kind.vercel.app/",
+            image:outofkind
         },
         {
-            name:"Melody: Music Player",
-            description:"Your Ultimate Ad-Free Music Companion! Developed with React Native and harnessing the power of JioSavan's Unofficial API, Melody offers you a seamless music streaming experience at your fingertips.",
-            skills:["React", "Tailwind CSS", "React Icons"],
-            github:Social.Github,
-            live:Social.Github
+            name:"Funngro: Landing Page",
+            description:"Welcome to Fungrow, a sleek and interactive landing page built with React. Fungrow is designed to showcase a modern approach to online learning and skill enhancement. This project demonstrates responsive design principles using styled-components for styling. ",
+            skills:["React", "Tailwind CSS", "Vite"],
+            github:"https://github.com/Infinite-Null/Funngro",
+            live:"https://funngro-xi.vercel.app/",
+            image:fungrow
         },
         {
-            name:"Melody: Music Player asd",
-            description:"Your Ultimate Ad-Free Music Companion! Developed with React Native and harnessing the power of JioSavan's Unofficial API, Melody offers you a seamless music streaming experience at your fingertips.",
-            skills:["React", "Tailwind CSS", "React Icons"],
-            github:Social.Github,
-            live:Social.Github
+            name:"O2 AI: Gemini API Integration",
+            description:"The React Native O2 AI app integrates Gemini AI API for diverse user experiences. Chat-based interactions offer access to information and AI-generated tools for composing emails, code, essays, and removing plagiarism. Users can also extract insights from images, enhancing productivity and problem-solving.",
+            skills:["React Native", "Gemini API", "AI Integration"],
+            github:"https://github.com/Infinite-Null/O2_AI",
+            live:"https://github.com/Infinite-Null/O2_AI/releases/tag/v2",
+            image:o2
         },
     ]
     return (
